08497a637fab2403b3c9df136f82f4a849366f70
BZemba87 <zembabernadette@gmail.com>
2023-01-27 12:00:23 +0000

    add class Meta to models.py

--- profiles/models.py
+++ profiles/models.py
@@ -9,4 +9,11 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
+        upload_to='images/', default='../default_profile_bxtylp'
+    )
+
+class Meta:
+    ordering = ['created_at']
+
+def __str__(self):
+        return f"{self.owner}'s profile"
