9f80ede6c2ba50d45f3d3e98ce37299e99029ed9
BZemba87 <zembabernadette@gmail.com>
2023-01-28 15:28:52 +0000

    added log in/log out views and is_owner field to ProfileSerializer

--- lastseenapi/permissions.py
+++ lastseenapi/permissions.py
@@ -2,7 +2,10 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-  def has_object_permission(self, request, view, obj):
-    if request.method in permissions.SAFE_METHODS:
-      return True
-    return obj.owner == request.user
+    """
+    User read only access if not profile owner
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
