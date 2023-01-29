56f1e9e3a1069bd0dbd9467c640002872006fb33
BZemba87 <zembabernadette@gmail.com>
2023-01-29 09:28:22 +0000

    add like list to views.py in likes app and urlpatterns to urls.py in likes app

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from lastseenapi.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    Lists likes and logged in users can create likes
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+    
