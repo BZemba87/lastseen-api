7d7d232cc2f97b3bdce2cbd889baa363b2b0fcc4
BZemba87 <zembabernadette@gmail.com>
2023-01-29 10:00:56 +0000

    add perform_create method to likes views.py

--- likes/views.py
+++ likes/views.py
@@ -12,6 +12,9 @@ class LikeList(generics.ListCreateAPIView):
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
     """
@@ -20,4 +23,3 @@ class LikeDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
-    
