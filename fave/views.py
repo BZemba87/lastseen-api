5dc85a1fdb275996dd1e5c114b5c29b8cbee4195
BZemba87 <zembabernadette@gmail.com>
2023-04-29 13:24:56 +0000

    add views to Fave views.py file and permissions

--- fave/views.py
+++ fave/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from lastseenapi.permissions import IsOwnerOrReadOnly
+from .models import Fave
+from .serializers import FaveSerializer
 
-# Create your views here.
+
+class FaveList(generics.ListCreateAPIView):
+    '''
+    Users can Fave and view Captions.
+    '''
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FaveSerializer
+    queryset = Fave.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FaveDetail(generics.RetrieveDestroyAPIView):
+    '''
+    Fave can be retrieved and deleted.
+    '''
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FaveSerializer
+    queryset = Fave.objects.all()
