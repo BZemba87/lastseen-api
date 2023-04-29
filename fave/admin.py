429dfbe4292fe38e29f5099aec9a89249e334483
BZemba87 <zembabernadette@gmail.com>
2023-04-29 12:10:14 +0000

    add fave model and registered to admin.py

--- fave/admin.py
+++ fave/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fave
+
+admin.site.register(Fave)
 
 # Register your models here.
--- fave/models.py
+++ fave/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from caption.models import Caption
 # Create your models here.
+
+class Fave(models.model):
+    """
+    Fave model is related to Owner and Caption model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.ForeignKey(Caption, related_name='fave',
+                                on_delete=models.CASCADE
+                                )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'caption']
+
+    def __str__(self):
+        return f'{self.owner} {self.caption}'
+
