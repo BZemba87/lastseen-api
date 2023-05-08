1fba024743b9fa43fd9c796b85f8ebb47983508c
BZemba87 <zembabernadette@gmail.com>
2023-05-08 20:04:52 +0000

    combined both lines into single perform_create method in views.py

--- captions/views.py
+++ captions/views.py
@@ -41,10 +41,7 @@ class CaptionList(generics.ListCreateAPIView):
     ]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(location=self.request.data['location'])
+        serializer.save(owner=self.request.user, location=self.request.data['location'])
 
 
 class CaptionDetail(generics.RetrieveUpdateDestroyAPIView):
